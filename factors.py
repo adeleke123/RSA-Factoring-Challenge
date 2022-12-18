f1b9e0a67497cfe5392b628443fe5af057379a46
Bakare Muideen Adeleke <bakare1234@gmail.com>
2022-12-18 13:47:27 -0800

    Task completed

--- factors.py
+++ factors.py
@@ -1,17 +1,19 @@
+#/usr/bin/env python3
+
 import sys
 
 def factorize(n):
-    for i in range(2, n):
+    for i in range(2, n+1):
         if n % i == 0:
-            return (i, n // i)
-    return (n, 1)
+            return (i, n//i)
 
-# Read numbers from the input file
+# Read the numbers from the file
 with open(sys.argv[1], 'r') as f:
-    numbers = [int(line.strip()) for line in f]
+    numbers = f.read().splitlines()
 
-# Factorize each number and print the result
+# Factorize each number and output the result
 for n in numbers:
+    n = int(n)
     p, q = factorize(n)
-    print(f"{n}={p}*{q}")
+    print(f'{n}={p}*{q}')
 
