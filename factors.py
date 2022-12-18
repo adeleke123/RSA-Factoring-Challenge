f8fcb22c3675f6755c0126452a3dc9d208a6f215
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-12-19 00:12:12 +0100

    Task completed

--- factors.py
+++ factors.py
@@ -1,20 +1,21 @@
 #/usr/bin/env python3
-
 import sys
 
-def factorize(n):
-    for i in range(2, n):
-        if n % i == 0:
-            return (i, n//i)
-    return (n, 1)
 
-def main():
-    # Open the file and read each line
-    with open(sys.argv[1], 'r') as f:
-        for line in f:
-            n = int(line)
-            p, q = factorize(n)
-            print(f"{n}={p}*{q}")
+def primef(n):
+    if n <= 3:
+        return int(n)
+    if n % 2 == 0:
+        return 2
+    elif n % 3 == 0:
+        return 3
+    else:
+        for i in range(5, int((n)**0.5) + 1, 6):
+            if n % i == 0:
+                return int(i)
+            if n % (i + 2) == 0:
+                return primef(n/(i+2))
+    return int(n)
 
-if __name__ == "__main__":
-    main()
+
+print(primef(int(sys.argv[1])))
